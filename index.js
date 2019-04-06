d0506befb0fcb91532624b30e8f6522877db068a
krzeelzb <krzeczekelzbieta@gmail.com>
2019-04-06 19:12:20 +0200

    slow port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { WebhookClient } = require("dialogflow-fulfillment");
 const { Card, Suggestion } = require("dialogflow-fulfillment");
+const {Permission, dialogflow} = require("actions-on-google");
+const dialogflowApp = dialogflow({debug: true});
 const app = express();
 
 app.get("/", (req, res) => res.send("online"));
@@ -16,9 +18,38 @@ app.post("/dialogflow", express.json(), (req, res) => {
         agent.add("Z domu czy z obecnej lokalizacji?");
     }
 
+    function shopingHourLocationHere() {
+        console.log(here);
+        const options = {
+            context: 'Aby odpowiedzieć ',
+            // Ask for more than one permission. User can authorize all or none.
+            permissions: ['NAME', 'DEVICE_PRECISE_LOCATION'],
+        };
+        agent.add(new Permission(options));
+    }
+
+
+    // 'ask_for_permission_confirmation': (conv, params, confirmationGranted) => {
+    //     const {location} = conv.device;
+    //     const {name} = conv.user;
+    //     if (confirmationGranted) {
+    //         if (name) {
+    //             conv.ask(`I'll send the driver your way now ${name.display}.`);
+    //         }
+    //         if (location) {
+    //             // const { latitude, longitude } = location.coordinates;
+    //             // you can uncomment the above lines and use the latitude and longitude
+    //         }
+    //     } else {
+    //         conv.ask(`Okay, yeah that's fine. I... didn't really want it anyway.`);
+    //     }
+    // },
+    // }
+
   let intentMap = new Map();
   intentMap.set("Default Welcome Intent", welcome);
   intentMap.set("shopping on specific hour", shopingHour);
+  intentMap.set("shopping on specific hour - here", shopingHourLocationHere);
   agent.handleRequest(intentMap);
 });
 
