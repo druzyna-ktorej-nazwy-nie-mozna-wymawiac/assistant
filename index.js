385b306d257d94ceb088891d09e365b240cf7324
krzeelzb <krzeczekelzbieta@gmail.com>
2019-04-06 16:31:12 +0200

    shopping hour

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ app.post("/dialogflow", express.json(), (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
 
   function welcome() {
-    agent.add("Wiataj w QWise");
+    agent.add("Witaj w QWise");
   }
+
+    function shopingHour(params) {
+        console.log(params.parameters.when);
+        agent.add("Z domu czy z obecnej lokalizacji?");
+    }
 
   let intentMap = new Map();
   intentMap.set("Default Welcome Intent", welcome);
+  intentMap.set("shopping on specific hour", shopingHour);
   agent.handleRequest(intentMap);
 });
 
